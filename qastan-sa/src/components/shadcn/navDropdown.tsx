8343f105363d5be350e1f90befd87f4dcaabcdc4
SalmaAbied <114472834+SalmaAbied@users.noreply.github.com>
2024-05-06 15:52:22 +0200

    bezig met hero

--- qastan-sa/src/components/shadcn/navDropdown.tsx
+++ qastan-sa/src/components/shadcn/navDropdown.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useLocation } from "react-router-dom";
-import * as React from "react"
+import React, { useState } from "react";
 import { cn } from "../../utils"
 import {
   NavigationMenu,
@@ -40,14 +40,22 @@ const components: { title: string; href: string; description: string }[] = [
 
 export function NavigationMenuDemo() {
     const location = useLocation();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+    const handleMenuToggle = () => {
+      setIsMenuOpen(!isMenuOpen);
+    };
   return (
     <NavigationMenu>
       <NavigationMenuList>
         <NavigationMenuItem>
-          <NavigationMenuTrigger><a href="/Oplossingen" className="text-base">Oplossingen</a></NavigationMenuTrigger>
+          <NavigationMenuTrigger>
+            <a href="/Oplossingen" className="text-base" onClick={handleMenuToggle}>
+              Oplossingen
+            </a>
+          </NavigationMenuTrigger>
           <NavigationMenuContent>
-            <ul className="bg-white grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+            <ul className="bg-white grid w-[300] gap-3 p-4 sm:w-[500px] md:grid-cols-2 lg:w-[600px] ">
               {components.map((component) => (
                 <ListItem
                   key={component.title}
@@ -70,12 +78,12 @@ const ListItem = React.forwardRef<
   React.ComponentPropsWithoutRef<"a">
 >(({ className, title, children, ...props }, ref) => {
   return (
-    <li>
+    <li className="hover:bg-orange-100">
       <NavigationMenuLink asChild>
         <a
           ref={ref}
           className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors",
             className
           )}
           {...props}
