9bcb07eb6a0d872451dfcedb9839bc394b96fe18
SalmaAbied <114472834+SalmaAbied@users.noreply.github.com>
2024-05-07 16:27:19 +0200

    hero 'klaar' nog niet responsive

--- qastan-sa/src/components/Hero.tsx
+++ qastan-sa/src/components/Hero.tsx
@@ -39,7 +39,7 @@ import Typed from 'typed.js';
 import left from '../img/left.png'
 import middle from '../img/middle.png'
 import right from '../img/right.png'
-import puffin from '../img/puffin.png'
+import puffin from '../img/puffin.jpeg'
 
 
 const Hero: React.FC = () => {
@@ -62,16 +62,16 @@ const Hero: React.FC = () => {
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 place-items-center p-10 min-h-screen overflow-hidden">
       <div className="info flex flex-col items-left gap-5 p-10 md:p-20">
-        <p className="p1 text-3xl md:text-5xl lg:text-7xl h-20">Helemaal</p>
+        <p className="text-3xl md:text-5xl lg:text-7xl h-20">Helemaal</p>
         <p className="p2 text-3xl md:text-5xl lg:text-7xl font-bold text-orange-500 h-20 mb-5">digitaal</p>
         <p className="text-lg md:text-xl lg:text-2xl text-center"></p>
-        <a href="#content" className="button explore">Neem contact op</a>
+        <a href="/Support" className='w-fit text-white border-2 bg-orange-500 hover:border-2 border-orange-500 hover:bg-white hover:text-orange-500 font-medium px-4 h-8 m-2 flex items-center'>Neem contact op</a>      
       </div>
       <div className="flex flex-col md:flex-row items-center md:justify-end">
-        <img className="" src={puffin} alt="puffin" />
-        <img className="max-w-full w-56 mr-2 mb-5 md:mb-0 " src={left} alt="left side puffin" />
-        <img className="max-w-full w-56 -translate-y-6 mb-5 md:mb-0" src={middle} alt="middle side puffin" />
-        <img className="max-w-full w-56 ml-2 mb-5 md:mb-0" src={right} alt="right side puffin" />
+        <img className="md:hidden" src={puffin} alt="puffin" />
+        <img className="hidden md:block max-w-full w-56 mr-2 mb-5 md:mb-0 " src={left} alt="left side puffin" />
+        <img className="hidden md:block max-w-full w-56 -translate-y-6 mb-5 md:mb-0" src={middle} alt="middle side puffin" />
+        <img className="hidden md:block max-w-full w-56 ml-2 mb-5 md:mb-0" src={right} alt="right side puffin" />
       </div>
     </section>
   );
